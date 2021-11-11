95a2b5a36b844adf08dff06b127c28bd229aeab4
lxstchance <kirpichnikovstas@gmail.com>
2021-11-11 12:31:36 +0300

    11.11

--- js/main.js
+++ js/main.js
@@ -6,10 +6,25 @@ $(function () {
     $('.rightside-menu').addClass('rightside-menu--close')
   });
 
+    $('.scroll-points__link').on('click', function () {
+    $('.scroll-points__link').removeClass('scroll-points__link--active')
+  });
+
+  $(".scroll-points__link").click(function () {
+     $(this).toggleClass("scroll-points__link--active");
+   });
+
+
+
+
   $('#fullpage').fullpage({
 		autoScrolling:true,
 		scrollHorizontally: true,
     sectionSelector:'.section',
-    
+    scrollOverflow: true,
+    anchors: ['aplication', 'about', 'portfolio', 'offer', 'benefits', 'spec-offer', 'footer'],
+	   menu: '#scroll-nav',
 	});
+
+
 });
